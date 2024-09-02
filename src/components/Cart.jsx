3caa91d1e6319efc0ccdb7a648d71e80861bba3f
Andres Puello <andresfpuello16@gmail.com>
2024-09-01 23:18:45 -0400

    Deploying nissy2

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,37 +1,26 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { CartContext } from '../Context/CartContext'
+import React, { useContext, useEffect } from 'react';
+import { CartContext } from '../Context/CartContext';
 import "../cssfolder/Cart.css";
-// import close from "../icons/x.png";
-// import cart from "../icons/shopping.svg";
+import close from "../icons/x.png";
 import ItemCart from './ItemCart';
 import { Link } from 'react-router-dom';
 
-const Cart = ({ cartOpen, setProductsLength }) => {
+const Cart = ({ cartOpen, setCartOpen }) => {
+    const { cartItems } = useContext(CartContext);
 
-    const { cartItems } = useContext(CartContext)
-
-    useEffect(() => {
-        setProductsLength(
-            cartItems.reduce((previous, current) => previous + current.amount, 0)
-        )
-    }, [cartItems])
-
-    const total = cartItems.reduce((previous, current) => previous + current.amount * current.price, 0)
+    const total = cartItems.reduce((previous, current) => previous + current.amount * current.price, 0);
 
     return (
-        <div className={cartItems && cartOpen ? "cart" : "cart-Close"}>
-
-
+        <div className={cartOpen ? "cart open" : "cart"}>
+            <img className='Close-cart' onClick={() => setCartOpen(false)} src={close} alt="close Icon" />
             <h2 className='Title-cart'>Tu carrito</h2>
-
-            {cartItems.length === 0 ? <p className='cart-vacio'>Tu carrito esta vacío.</p> : (
+            {cartItems.length === 0 ? <p className='cart-vacio'>Tu carrito está vacío.</p> : (
                 <div className='productsContainer'>
                     {cartItems.map((item, i) => (
                         <ItemCart key={i} item={item} />
                     ))}
                 </div>
             )}
-
             <div className="Ordering">
                 <div className="Ordering-total">
                     <p className='cart-total'>Subtotal</p>
@@ -41,30 +30,65 @@ const Cart = ({ cartOpen, setProductsLength }) => {
                 <button><Link to="/checkout">Ir a Checkout</Link></button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Cart
-
-// {/* <div className={cartItems && cartOpen ? "cart" : "cart-Close"}>
+export default Cart;
 
 
-// <h2 className='Title-cart'>Tu carrito</h2>
 
-// {cartItems.length === 0 ? <p className='cart-vacio'>Tu carrito esta vacío.</p> : (
-//     <div className='productsContainer'>
-//         {cartItems.map((item, i) => (
-//             <ItemCart key={i} item={item} />
-//         ))}
-//     </div>
-// )}
 
-// <div className="Ordering">
-//     <div className="Ordering-total">
-//         <p className='cart-total'>Subtotal</p>
-//         <p className='cart-total'>${total}</p>
-//     </div>
-//     <p>El costo de envío es totalmente GRATIS!</p>
-//     <button>REALIZAR PEDIDO</button>
-// </div>
-// </div> */}
+
+
+
+
+
+
+// import React, { useContext, useEffect, useState } from 'react'
+// import { CartContext } from '../Context/CartContext'
+// import "../cssfolder/Cart.css";
+// // import close from "../icons/x.png";
+// // import cart from "../icons/shopping.svg";
+// import ItemCart from './ItemCart';
+// import { Link } from 'react-router-dom';
+
+// const Cart = ({ cartOpen, setProductsLength }) => {
+
+//     const { cartItems } = useContext(CartContext)
+
+//     useEffect(() => {
+//         setProductsLength(
+//             cartItems.reduce((previous, current) => previous + current.amount, 0)
+//         )
+//     }, [cartItems])
+
+//     const total = cartItems.reduce((previous, current) => previous + current.amount * current.price, 0)
+
+//     return (
+//         <div className={cartItems && cartOpen ? "cart" : "cart-Close"}>
+
+
+//             <h2 className='Title-cart'>Tu carrito</h2>
+
+//             {cartItems.length === 0 ? <p className='cart-vacio'>Tu carrito esta vacío.</p> : (
+//                 <div className='productsContainer'>
+//                     {cartItems.map((item, i) => (
+//                         <ItemCart key={i} item={item} />
+//                     ))}
+//                 </div>
+//             )}
+
+//             <div className="Ordering">
+//                 <div className="Ordering-total">
+//                     <p className='cart-total'>Subtotal</p>
+//                     <p className='cart-total'>${total}</p>
+//                 </div>
+//                 <p>El costo de envío es totalmente GRATIS!</p>
+//                 <button><Link to="/checkout">Ir a Checkout</Link></button>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Cart
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,90 +1,114 @@
-import React, { useContext, useState } from 'react'
-import "../cssfolder/Header.css";
-// import { Link } from 'react-router-dom';
-import logo from "../icons/logoBlessed.png";
-// import Cart from './Cart'
-import close from "../icons/x.png";
-import cart from "../icons/shopping.svg";
-import { Link } from 'react-router-dom';
-import Cart from './Cart';
+import React, { useState, useContext, useEffect } from 'react';
 import { CartContext } from '../Context/CartContext';
+import "../cssfolder/Header.css";
+import cartIcon from "../icons/shopping.svg";
+import logo from "../icons/logoBlessed.png"; // Asegúrate de importar el logo
+import Cart from './Cart';
 
 const Header = () => {
+    const [cartOpen, setCartOpen] = useState(false);
+    const { cartItems } = useContext(CartContext);
+    const [shrink, setShrink] = useState(false);
 
-    const [cartOpen, setCartOpen] = useState(false)
-    const [productsLength, setProductsLength] = useState(0)
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setShrink(true);
+            } else {
+                setShrink(false);
+            }
+        };
 
-    const { cartItems } = useContext(CartContext)
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
-        <div className='Header'>
-
-            <Link to="/"><img className="logo" src={logo} alt="" /> </Link>
-
-            <div className='ul-container'>
-                <ul className="navbar">
-
-                    {/* <li><Link className="active" to="/">Home</Link></li> */}
-
-                    <li className={!cartOpen ? "handbag-close" : "handbag"}>
-                        <div className='buttonCartContainer'>
-                            <div className='buttonCart'>
-                                {!cartOpen ? (
-                                    <img onClick={() => setCartOpen(!cartOpen)} src={cart} alt="cart Icon" />
-                                ) : (
-                                    <div className='Header-cart'>
-                                        <img className='Close-cart' onClick={() => setCartOpen(!cartOpen)} src={close} alt="close Icon" />
-                                        <div onClick={() => setCartOpen(!cartOpen)} className={cartOpen ? "Overlay-visible" : "Overlay"}></div>
-                                        <Cart cartOpen={cartOpen} setProductsLength={setProductsLength} />
-                                    </div>
-                                )}
-                            </div>
-                            <div>
-                                {!cartOpen && cartItems.length != 0 && (
-                                    <div className='productsNumber'>{cartItems.length}</div>
-                                )}
-                            </div>
-                        </div>
-                    </li>
-                </ul>
+        <header className={`Header ${shrink ? 'shrink' : ''}`}>
+            <div className='LogoContainer'>
+                <img src={logo} alt="Logo" className='Logo' />
             </div>
 
-        </div>
-    )
-}
-
-export default Header
-
-
-{/* <div className='Header'>
-
-<Link to="/"><img className="logo" src={logo} alt="" /> </Link>
-
-<div className='ul-container'>
-    <ul className="navbar">
-
-        <li><Link className="active" to="/">Home</Link></li>
-
-        <li className={!cartOpen ? "handbag-close" : "handbag"}>
             <div className='buttonCartContainer'>
                 <div className='buttonCart'>
-                    {!cartOpen ? (
-                        <img onClick={() => setCartOpen(!cartOpen)} src={cart} alt="cart Icon" />
-                    ) : (
-                        <div className='Header-cart'>
-                            <img className='Close-cart' onClick={() => setCartOpen(!cartOpen)} src={close} alt="close Icon" />
-                            <div onClick={() => setCartOpen(!cartOpen)} className={cartOpen ? "Overlay-visible" : "Overlay"}></div>
-                            <Cart cartOpen={cartOpen} setProductsLength={setProductsLength} />
-                        </div>
-                    )}
-                </div>
-                <div>
-                    {!cartOpen && cartItems.length != 0 && (
+                    <img onClick={() => setCartOpen(!cartOpen)} src={cartIcon} alt="cart Icon" />
+                    {cartItems.length !== 0 && (
                         <div className='productsNumber'>{cartItems.length}</div>
                     )}
+                    {cartOpen && <div className="Overlay" onClick={() => setCartOpen(false)}></div>}
+                    <Cart cartOpen={cartOpen} setCartOpen={setCartOpen} />
                 </div>
             </div>
-        </li>
-    </ul>
-</div>
-</div> */}
+        </header>
+    );
+};
+
+export default Header;
+
+
+
+
+
+
+
+
+
+
+// import React, { useContext, useState } from 'react'
+// import "../cssfolder/Header.css";
+// // import { Link } from 'react-router-dom';
+// import logo from "../icons/logoBlessed.png";
+// // import Cart from './Cart'
+// import close from "../icons/x.png";
+// import cart from "../icons/shopping.svg";
+// import { Link } from 'react-router-dom';
+// import Cart from './Cart';
+// import { CartContext } from '../Context/CartContext';
+
+// const Header = () => {
+
+//     const [cartOpen, setCartOpen] = useState(false)
+//     const [productsLength, setProductsLength] = useState(0)
+
+//     const { cartItems } = useContext(CartContext)
+
+//     return (
+//         <div className='Header'>
+
+//             <Link to="/"><img className="logo" src={logo} alt="" /> </Link>
+
+//             <div className='ul-container'>
+//                 <ul className="navbar">
+
+//                     {/* <li><Link className="active" to="/">Home</Link></li> */}
+
+//                     <li className={!cartOpen ? "handbag-close" : "handbag"}>
+//                         <div className='buttonCartContainer'>
+//                             <div className='buttonCart'>
+//                                 {!cartOpen ? (
+//                                     <img onClick={() => setCartOpen(!cartOpen)} src={cart} alt="cart Icon" />
+//                                 ) : (
+//                                     <div className='Header-cart'>
+//                                         <img className='Close-cart' onClick={() => setCartOpen(!cartOpen)} src={close} alt="close Icon" />
+//                                         <div onClick={() => setCartOpen(!cartOpen)} className={cartOpen ? "Overlay-visible" : "Overlay"}></div>
+//                                         <Cart cartOpen={cartOpen} setProductsLength={setProductsLength} />
+//                                     </div>
+//                                 )}
+//                             </div>
+//                             <div>
+//                                 {!cartOpen && cartItems.length != 0 && (
+//                                     <div className='productsNumber'>{cartItems.length}</div>
+//                                 )}
+//                             </div>
+//                         </div>
+//                     </li>
+//                 </ul>
+//             </div>
+
+//         </div>
+//     )
+// }
+
+// export default Header
